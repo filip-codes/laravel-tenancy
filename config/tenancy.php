b09f00df5f6e826b514511c2149e98ce0668955d
filip-codes <filip@bitecorn.com>
2024-03-21 06:28:25 +0100

    central_domain change

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,7 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'tenancy.test',
+        'laravel-tenancy.test',
     ],
 
     /**
